d69d1f56ff54b73106c03aa04ddcb7e2da64e39a
QingYu2017 <30379078+QingYu2017@users.noreply.github.com>
2019-04-09 20:14:57 +0800

    Update and rename bestpay_Auth V1.0.py to worstpay_Auth V1.0.py

--- worstpay_Auth V1.0.py
+++ worstpay_Auth V1.0.py
@@ -6,14 +6,14 @@
 #Date:2018-07-04
 
 import requests as r,json
-import cls_bestpay
+import cls_worstpay
 
-pkey = cls_bestpay.obj_bestpay()
+pkey = cls_worstpay.obj_worstpay()
 (identityNumber, merchantNo, mobilePhone, userName)=(pkey.identityNumber, pkey.merchantNo, pkey.mobilePhone, pkey.userName)
 pkey.string = "identityNumber=%s&merchantNo=%s&mobilePhone=%s&userName=%s"%(identityNumber, merchantNo, mobilePhone, userName)
 pkey.get_pkey()
 sign=pkey.get_sign()
-url="http://116.xxx.xxx.160:18002/mapi/gtsIdentityAutCa"
+url="http://xxx.xxx.xxx.xxx:1xxx2/mapi/gtsxxxxxxxxAutCa"
 headers = {"Content-Type": "application/json"}
 payload={
         "identityNumber": identityNumber,
@@ -29,4 +29,4 @@ print(s)
 #调试返回json
 #{'errorCode': None, 'errorMsg': None, 'result': {'isAgreement': True, 'isAgreementReason': '恭喜您通过认证'}, 'success': True}
 #{'errorCode': None, 'errorMsg': None, 'result': {'isAgreement': False, 'isAgreementReason': '您的证件号未通过验证'}, 'success': True}
-#{'errorCode': 'API100001', 'errorMsg': '签名认证失败', 'result': None, 'success': False}
+#{'errorCode': 'xxx100001', 'errorMsg': '签名认证失败', 'result': None, 'success': False}
